5220520946271dcccd764352adc1e0a61110e816
KristenChing <kristenching1@gmail.com>
2017-05-11 10:17:59 -0700

    speed added :)

--- snek.py
+++ snek.py
@@ -14,17 +14,15 @@ snaketail.speed(0)
 snaketail.hideturtle()
 coords = []
 turns = 0
-speed = 1
-oldx = 0
-oldy = 0
-oldoldx = 0
-oldoldy = 0
+speed = 2
+apple = 0
+tailspeed = 1
 turnedLeft = False
 turnedRight = False
 timer = 5
 def addspeed():
-    global speed
-    speed = 2
+    global tailspeed
+    tailspeed = 2
 def resetcoords():
     global x
     x = snake.xcor()
@@ -40,6 +38,8 @@ def left():
     snake.left(90)
     resetcoords()
     turnedLeft = True
+    global apple
+    apple = 1
 def right():
     global turnedRight
     global turns
@@ -47,12 +47,21 @@ def right():
     snake.right(90)
     resetcoords()
     turnedRight = True
+    global apple
+    apple = 1
 turtle.listen()
 turtle.onkey(left, "Left")
 turtle.onkey(right, "Right")
 while True:
-    snake.forward(2)
-    snaketail.forward(speed)
+    snake.forward(speed)
+    snaketail.forward(tailspeed)
+    print(speed)
+    print("tail", tailspeed)
+    print("turns", turns)
+    print("coords list", coords)
+    print("snake coords", snake.xcor(), snake.ycor()) 
+    print("snaketail coords", snaketail.xcor(), snaketail.ycor())
+    print("\n")
     x = snake.xcor()
     y = snake.ycor()
     for x in range(turns):
@@ -60,11 +69,17 @@ while True:
             if turnedLeft == True:
                 snaketail.left(90)
                 addspeed()
+                tailspeed = speed
                 turnedLeft = False
             if turnedRight == True:
                 snaketail.right(90)
                 addspeed()
+                tailspeed = speed
                 turnedRight = False
+        if (apple == 1):
+                oldspeed = speed
+                speed = oldspeed + 0.5
+                apple = 0                
 
 #-------------------------------------debug code-------------------------------
 ##    print("turns", turns)
